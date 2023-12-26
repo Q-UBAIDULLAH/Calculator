0afe418a10009d2c5fc0ff5a93d30d6323821e96
Q-UBAIDULLAH <qaziubaidullah5@gmail.com>
2023-12-27 01:21:34 +0500

    add files

---
+++ calculator/index.js
@@ -0,0 +1,15 @@
+var inputValue = document.getElementById('Input-field')
+function getNum(num) {
+    inputValue.value += num
+}
+
+function calculate() {
+    var value = inputValue.value
+    var result = eval(value)
+    inputValue.value = result
+
+}
+
+function cls() {
+    inputValue.value = ''
+}
